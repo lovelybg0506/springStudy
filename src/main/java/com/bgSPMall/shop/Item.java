7138b25b426c6bb6b658f2386b78eba9f481274a
bgkang <lovelybg0506@gmail.com>
2024-10-08 14:52:58 +0900

    20241008_1

--- src/main/java/com/bgSPMall/shop/Item.java
+++ src/main/java/com/bgSPMall/shop/Item.java
@@ -10,17 +10,23 @@ import lombok.ToString;
 @ToString // toString 함수 사용가능
 @Getter @Setter
 public class Item {
+
+    // 상품 id
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 상품명
     @Column(nullable = false)
     private String title;
 
+    // 가격
     @Column(nullable = false)
     private Integer price;  // column용 변수에서는 int 대신 Integer 사용
+
+    // 상품설명
+    private String description;
 
 //    public String toString(){
 //        return this.title + this.price;
 //    }
 }
-
